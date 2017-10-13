32323fbd669365f4116e41fe3e184002ed7714f0
ios <572152439@qq.com>
2017-10-13 19:50:16 +0800

    使用AdminLTE这个Bootstrap模板搭建主页的框架

--- src/main/java/com/qf/PersonalErpApplication.java
+++ src/main/java/com/qf/PersonalErpApplication.java
@@ -6,6 +6,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PersonalErpApplication {
 
+//	@Bean
+//	public EmbeddedServletContainerCustomizer containerCustomizer() {
+//
+//		return new EmbeddedServletContainerCustomizer() {
+//			@Override
+//			public void customize(ConfigurableEmbeddedServletContainer container) {
+//
+////				ErrorPage error401Page = new ErrorPage(HttpStatus.UNAUTHORIZED, "/401.html");
+//				ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/404.html");
+//				ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/500.html");
+//
+//				container.addErrorPages(error404Page, error500Page);
+//			}
+//		};
+//	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(PersonalErpApplication.class, args);
 	}
